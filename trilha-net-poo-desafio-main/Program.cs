6fc873c5e7c3be59236368e9b0a34f4ad9afed57
kaiofe18 <142911532+KAZIIN-ops@users.noreply.github.com>
2023-11-07 18:35:21 -0300

    Concluido

---
+++ trilha-net-poo-desafio-main/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using DesafioPOO.Models;
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia s1= new Nokia(numero:"12",modelo:"galaxy",imei:"123",memoria:24);
+s1.ReceberLigacao();
+s1.InstalarAplicativo("AppContext");
+Iphone s2 = new Iphone(numero:"123",modelo:"ai",imei:"123",memoria:25);
+s2.Ligar();
+s2.ReceberLigacao();
+s2.InstalarAplicativo("Android kkkk");
+
+
